361a5cb1b9f2baf9b275fd083a0f705b569b975e
Meg-geek <m.khlybova@g.nsu.ru>
2020-03-23 18:25:26 +0700

    add gain ratio diagram frame

---
+++ Lab1/src/main/java/lab1/neural/nsu/well/view/GainRatioDiagramPanel.java
@@ -0,0 +1,70 @@
+package lab1.neural.nsu.well.view;
+
+import lab1.neural.nsu.well.GainParameter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.lang.Math.max;
+
+public class GainRatioDiagramPanel extends JPanel {
+    private final int AXIS_X;
+    private final int AXIS_Y;
+    private final int INDENT;
+    private final int TEXT_INDENT;
+    private List<GainParameter> parameterGain;
+    private final int LINE_COEF = 2;
+
+
+    public GainRatioDiagramPanel(List<GainParameter> parameterGain, int frameHeight, int frameWidth) {
+        this.parameterGain = parameterGain;
+        this.INDENT = max(frameHeight / 15, frameWidth / 15);
+        this.TEXT_INDENT = INDENT / 10;
+        this.AXIS_X = frameWidth - INDENT;
+        this.AXIS_Y = frameHeight - INDENT;
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.drawLine(INDENT, AXIS_Y, INDENT, INDENT);
+        graphics2D.drawLine(INDENT, AXIS_Y, AXIS_X, AXIS_Y);
+        drawGainRatioDiagram(graphics2D);
+    }
+
+    private void drawGainRatioDiagram(Graphics2D graphics2D) {
+        float maxGainRatio = Collections.max(parameterGain,
+                Comparator.comparing(GainParameter::getGainRatio))
+                .getGainRatio();
+        int yIndent = (AXIS_Y - INDENT) / (parameterGain.size() * 2);
+        int y = AXIS_Y - yIndent;
+        for (GainParameter gainParameter : parameterGain) {
+            int rectWidth = (int) ((gainParameter.getGainRatio() / maxGainRatio) * (AXIS_X - INDENT));
+            graphics2D.setColor(Color.BLUE);
+            graphics2D.fillRect(INDENT, y, rectWidth, yIndent);
+            graphics2D.setColor(Color.BLACK);
+            graphics2D.drawString(gainParameter.getGainRatio().toString(), INDENT + rectWidth, y + yIndent);
+            drawParameterName(graphics2D,
+                    gainParameter.getParameterName(),
+                    TEXT_INDENT,
+                    y + yIndent,
+                    INDENT - TEXT_INDENT, yIndent);
+            y -= 2 * yIndent;
+        }
+    }
+
+    private void drawParameterName(Graphics2D graphics2D, String parameterName, int x, int y, int width, int height) {
+        if (graphics2D.getFontMetrics().stringWidth(parameterName) > width) {
+            String firstLine = parameterName.substring(0, parameterName.length() / LINE_COEF);
+            graphics2D.drawString(firstLine, x, y);
+            String secondLine = parameterName.substring(parameterName.length() / LINE_COEF);
+            graphics2D.drawString(secondLine, x, y + height);
+        } else {
+            graphics2D.drawString(parameterName, x, y);
+        }
+    }
+}
---
+++ Lab1/src/main/java/lab1/neural/nsu/well/view/GraphicPainter.java
@@ -0,0 +1,24 @@
+package lab1.neural.nsu.well.view;
+
+import lab1.neural.nsu.well.GainParameter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class GraphicPainter extends JFrame {
+    private final int FRAME_HEIGHT = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    private final int FRAME_WIDTH = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+
+    public GraphicPainter(List<GainParameter> parameterGains) {
+        super("Gain ratio diagram");
+        JPanel graphicPanel = new JPanel(new BorderLayout());
+        setContentPane(graphicPanel);
+        graphicPanel.add(new GainRatioDiagramPanel(parameterGains, FRAME_HEIGHT, FRAME_WIDTH), BorderLayout.CENTER);
+        graphicPanel.setBackground(Color.gray);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
